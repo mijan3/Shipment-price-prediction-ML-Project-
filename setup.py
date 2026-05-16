ee62f64aca5d619a57c89e83ff91913f77a901ec
Mijanur Rahman <110850337+mijan3@users.noreply.github.com>
2026-05-16 21:02:25 +0600

    requirements,setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name="shipment",
+    version="0.0.1",
+    author="Mijan",
+    author_email="mijan.rahman13062020@gmail.com",
+    description="A machine learning project for predicting shipment prices",
+    packages=find_packages(),
+    install_requires=[],
+)
+    
